57ea3d62b14e6c9e4c051fd2a5df5098734e6e25
Edvin Dzidic <edvindzidic2000@gmail.com>
2019-07-15 01:14:03 +0200

    Added more functionality to the home page.

--- app/MainViews/home.js
+++ app/MainViews/home.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Keyboard, StyleSheet, Text, View,Button,TouchableOpacity,TouchableWithoutFeedback} from 'react-native';
+import {Keyboard, StyleSheet, Text, View,Button,TouchableOpacity,TouchableWithoutFeedback,ScrollView} from 'react-native';
 import { NavigationEvents,createStackNavigator, createAppContainer,StackActions, NavigationActions,createBottomTabNavigator} from 'react-navigation';
 import AsyncStorage from '@react-native-community/async-storage';
 
@@ -16,6 +16,55 @@ export class mainHome extends Component{
         BMI:''
         };
       }
+
+       newDate = () => {
+        var date = JSON.stringify(new Date());
+        console.log(date);
+        var newDate = '';
+        for(var i = 12; i<=16;i++){
+            newDate += date[i];
+        }
+        console.log(newDate);
+        return newDate;
+    }
+
+    styleCard = () => {
+        const { BMI } = this.state;
+        if(BMI >= 0 && BMI <= 16){
+            return {borderColor:'red'}
+           }else if(BMI >= 16.1 && BMI <= 17){
+            return {borderColor:'orange'}
+           }else if(BMI >= 17.1 && BMI <= 18.5){
+            return {borderColor:'#F8EC00'}
+           }else if(BMI >= 18.6 && BMI <= 25){
+            return {borderColor:'rgb(0, 214, 64)'}
+           }else if(BMI >= 25.1 && BMI <= 30){
+            return {borderColor:'#F8EC00'}
+           }else if(BMI >= 30.1 && BMI <= 35){
+            return {borderColor:'orange'}
+           }else if(BMI >= 35.1){
+            return {borderColor:'red'}
+           }
+    }
+
+    returnText = () => {
+        const { BMI } = this.state;
+        if(BMI >= 0 && BMI <= 16){
+            return "severely thin";
+           }else if(BMI >= 16.1 && BMI <= 17){
+            return "moderately thin";
+           }else if(BMI >= 17.1 && BMI <= 18.5){
+            return "mildly thin";
+           }else if(BMI >= 18.6 && BMI <= 25){
+            return "normal";
+           }else if(BMI >= 25.1 && BMI <= 30){
+            return "overweight";
+           }else if(BMI >= 30.1 && BMI <= 35){
+            return "obese class I";
+           }else if(BMI >= 35.1){
+            return "obese class II";
+           }
+    }
 
     componentWillMount = async () => {
         
@@ -57,19 +106,21 @@ export class mainHome extends Component{
         />
          
           {this.state.view && <View style={{alignItems: 'center',height:'100%',backgroundColor:'rgb(126,174,252)'}}> 
-            <TouchableOpacity onPress={()=>this.props.navigation.navigate("cardOne")} style={[styles.card,{height:'40%',borderColor:'yellow',borderWidth:4}]}>
-            <Text style = {{fontSize:20}}> Your BMI is: </Text>
-            <Text style = {{fontSize:40}}> {this.state.BMI} </Text>
+            <TouchableOpacity onPress={()=>this.props.navigation.navigate("cardOne")} style={[styles.card,{height:'10%',borderWidth:3,backgroundColor:'white'},this.styleCard()]}>
+            <Text style = {{fontSize:27,fontFamily:'FugazOne-Regular',color:'rgb(30,30,30)'}}>You are {this.returnText()}! </Text> 
+            {/* <Text style = {{fontSize:25}}> Your specific BMI is: {this.state.BMI} </Text>
+            <Text> To learn more about what this means click this box! </Text> */}
             </TouchableOpacity>
-            <TouchableOpacity style={[styles.card]}>
+            <TouchableOpacity style={[styles.card,{height:'10%'}]}>
+            <Text style = {{fontSize:23,fontFamily:'FugazOne-Regular',color:'rgb(30,30,30)'}} onPress={()=>alert(new Date())}> Current waist size is: {this.newDate()} </Text>
             </TouchableOpacity>
-            <TouchableOpacity style={[styles.card]}>
+            <TouchableOpacity style={[styles.card,{height:'65%'}]}>
             </TouchableOpacity>
         </View> }
 
         {this.state.view === false &&  <View style={{height:'100%',width:'100%',alignItems: 'center',justifyContent:'center',backgroundColor:'rgb(126,174,252)'}}>
         <Text style={{color:'white',fontSize:30,fontFamily:'Acme-Regular',textAlign:'center'}}> 
-        Update your inromation in the profile tab! 
+        Update your information in the profile tab! 
         </Text>
         </View>
          }
@@ -91,6 +142,7 @@ export class cardOne extends Component{
     render(){
         return( 
         <View style={{alignItems: 'center',height:'100%',backgroundColor:'rgb(126,174,252)'}}> 
+        <Text> Your current waist size is: </Text>
         </View>
         );
     }
@@ -119,7 +171,7 @@ const styles = StyleSheet.create({
         width: '90%',
         alignItems: 'center',
         justifyContent: 'center',
-        height: '25%',
+        height: '15%',
         marginTop: 15,
         borderRadius: 10    
     }
